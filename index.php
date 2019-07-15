88376c74244336546e3070dcf69960a16fa4a603
ynikulyak <45584946+ynikulyak@users.noreply.github.com>
2019-07-14 21:11:04 -0700

    Update index.php

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
 	Gender: <input type="radio" name="gender" value="m"> Male
 			<input type="radio" name="gender" value="f"> Female<br><br>
 
-	Zip Code: <input type="text" name="zip" id="zip"><span id="zipError></span><br>
+	Zip Code: <input type="text" name="zip" id="zip"><span id="zipError"></span><br>
 	City: <span id="city"></span><br>
 	Latitude: <span id="latitude"></span> <br>
 	Longitude: <span id="longt"></span> <br><br>
